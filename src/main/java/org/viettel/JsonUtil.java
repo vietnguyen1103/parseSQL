5abd4aa35e66b24066946f4f97d665a981ae7a86
vietnguyen1103 <vietnguyen1103@gmail.com>
2024-08-20 15:57:09 +0700

    commit code

---
+++ src/main/java/org/viettel/JsonUtil.java
@@ -0,0 +1,11 @@
+package org.viettel;
+
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class JsonUtil {
+    public static Map<String, String> parse(String object) {
+        return new Gson().fromJson(object, Map.class);
+    }
+}
